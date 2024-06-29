97884c82487b735116eeca97e494162366e44d73
Haelyton <haelyton149@gmail.com>
2024-06-29 14:33:30 -0300

    Alteração - Variaveis - ContaBancaria

--- src/main/java/com/maicon_projeto_back/desafio_back_end/model/ContaBancaria.java
+++ src/main/java/com/maicon_projeto_back/desafio_back_end/model/ContaBancaria.java
@@ -1,13 +1,15 @@
 package com.maicon_projeto_back.desafio_back_end.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class ContaBancaria {
-    private long id;
+    private Integer id;
     private String nome;
     private String cpf;
     private String senha;
-    private Double saldo = 0.0;
     private LocalDate data;
+    private BigDecimal saldo = BigDecimal.ZERO;
+    
 
 }
